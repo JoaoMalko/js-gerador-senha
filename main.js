d50ff98eea04fc90b7b7a80ce35da48d9f9bc7dc
JoaoMalko <163422055+JoaoMalko@users.noreply.github.com>
2025-08-18 10:21:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -23,10 +23,27 @@ function aumentaTamanho(){
 }
 
 const campoSenha = document.querySelector('campo-senha');
-const letrasMaiusculas = 'abcdefghijklmnopqrstuvwxyz';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$%¨&*';
+
 gerasenha();
 
 function gerasenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+     if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+     }
+     if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+     }
+     if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+     }
     let senha = '';
     for (let i = 0, i < tamanhoSenha i++){
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
